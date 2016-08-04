ec8fdd9582f9412b40ff6d03a784962a316b6c38
JerrmLee <616701525@qq.com>
2016-08-05 00:22:25 +0800

    15
    fdf

--- src/com/bgy/crm/HelloBgy.java
+++ src/com/bgy/crm/HelloBgy.java
@@ -7,5 +7,8 @@ public class HelloBgy {
 		System.out.println("hello02");
 		System.out.println("helloEclipse");
 		System.out.println("helloEclipse222");
+		String str = "abc";
+		System.out.println("h88");
+		System.out.println("h66");
 	}
 }
